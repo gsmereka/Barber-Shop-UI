21c06436c5afb462fc82c725c0da40fc4038e1f6
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-04-12 18:33:57 -0300

    feat: update client models

--- src/app/clients/client.models.ts
+++ src/app/clients/client.models.ts
@@ -11,3 +11,41 @@ export interface ClientModelTable {
     email: string
     phone: string
 }
+
+export interface SaveClientRequest {
+    name: string
+    email: string
+    phone: string
+}
+
+export interface SaveClientResponse {
+    id: number
+    name: string
+    email: string
+    phone: string
+}
+
+export interface UpdateClientRequest {
+    id: number
+    name: string
+    email: string
+    phone: string
+}
+
+export interface UpdateClientResponse {
+    id: number
+    name: string
+    email: string
+    phone: string
+}
+
+export interface DetailClientResponse {
+    id: number
+    name: string
+    email: string
+    phone: string
+}
+
+export interface ListClientResponse {
+    clients: ClientModelTable[]
+}
