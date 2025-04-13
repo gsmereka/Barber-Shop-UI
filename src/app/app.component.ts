431f44c7beafa1a8d9854d0066c0802ebb137908
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-04-12 23:35:48 -0300

    feat: implments menu-bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { filter, map, Subscription } from 'rxjs';
 import { CardHeaderComponent } from './commons/components/card-header/card-header.component';
+import { MenuBarComponent } from './commons/components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardHeaderComponent],
+  imports: [RouterOutlet, CardHeaderComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
