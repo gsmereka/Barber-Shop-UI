a2f2dd75af60230990f7086cb3256f083926d82c
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-04-13 11:46:00 -0300

    feat: inplements dialog-manager.service

--- src/app/services/service.token.ts
+++ src/app/services/service.token.ts
@@ -1,7 +1,7 @@
 import { InjectionToken } from "@angular/core";
 import { ICLientService } from "./api-client/clients/iclients.service";
 import { ISnackbarManagerService } from "./isnackbar-manager.service";
-// import { IDialogManagerService } from "./idialog-manager.service";
+import { IDialogManagerService } from "./idialog-manager.service";
 // import { IScheduleService } from "./api-client/schedules/ischedules.service";
 
 export const SERVICES_TOKEN = {
@@ -10,5 +10,5 @@ export const SERVICES_TOKEN = {
         // SCHEDULE: new InjectionToken<IScheduleService>('SERVICES_TOKEN.HTTP.SCHEDULE'),
     },
     SNACKBAR: new InjectionToken<ISnackbarManagerService>('SERVICES_TOKEN.SNACKBAR'),
-    // DIALOG: new InjectionToken<IDialogManagerService>('SERVICES_TOKEN.DIALOG')
+    DIALOG: new InjectionToken<IDialogManagerService>('SERVICES_TOKEN.DIALOG')
 }
